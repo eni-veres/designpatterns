4b21c3e169938d884c4dea9c6d14498e26fb2aea
laviniadragoi14 <laviniadragoi14@yahoo.com>
2014-04-15 16:04:57 +0300

    GamePP

--- DesignPatternsGame/DesignPatternsGame/GamePP.cpp
+++ DesignPatternsGame/DesignPatternsGame/GamePP.cpp
@@ -1,11 +1,92 @@
 #include "GamePP.h"
 
+GamePP* GamePP::instance=NULL;
 
 GamePP::GamePP(void)
 {
+	b=ReversiBoard::getBoardInstance();
+	win=NONE;
+}
+
+GamePP* GamePP::getInstance()
+{
+	if (instance==NULL) {
+		instance=new GamePP();
+	}
+	return instance;
 }
 
 
+void GamePP::init(std::vector<std::vector<int>>& board, int stplayer)
+{
+	board=b->GetBoardState();
+	currentplayer=stplayer;
+}
+
+
+int GamePP::move(std::vector<std::vector<int>>& board, int i, int j)
+{
+	//human player move
+	if(currentplayer == WHITE){
+		if(player1.move(i,j))
+		{
+			SwitchTurn();
+		}
+		else
+		{
+			MessageBox(0,"Not valid move. Try again.","Error",0);
+		}
+	}
+	else {
+		if(player2.move(i,j))
+		{
+			SwitchTurn();
+		}
+		else
+		{
+			MessageBox(0,"Not valid move. Try again.","Error",0);
+		}
+	}
+	board=b->GetBoardState();
+
+	return win;
+}
+
+int GamePP::getPlayer()
+{
+	return currentplayer;
+}
+
+//change player, if player has no moves change again
+void GamePP::SwitchTurn()
+{
+	if(IsGameOver())
+		win=GetWinner();
+	else
+	{
+		currentplayer=3-currentplayer;
+		if(b->GetMoves(currentplayer).empty())
+			SwitchTurn();
+	}
+}
+
+//check if game is over
+bool GamePP::IsGameOver()
+{
+	return b->IsBoardFilled() || (b->GetMoves(WHITE).empty() && b->GetMoves(BLACK).empty());
+}
+
+//check who is the winner
+int GamePP::GetWinner()
+{
+	if(b->GetScore(WHITE)<b->GetScore(BLACK))
+		return BLACK;
+	return WHITE;
+}
+int GamePP::getScore(int player)
+{
+	return b->GetScore(player);
+}
 GamePP::~GamePP(void)
 {
 }
--- DesignPatternsGame/DesignPatternsGame/GamePP.h
+++ DesignPatternsGame/DesignPatternsGame/GamePP.h
@@ -1,10 +1,33 @@
+
 #pragma once
+#include<vector>
+#include"ReversiBoard.h"
+#include"ReversiState.h"
+#include"HumanPlayer.h"
+#include"ComputerPlayer.h"
+#include"windows.h"
 
 class GamePP
 {
+	static GamePP* instance;
 	//clasa Singleton
-public:
+	
+	ReversiBoard* b;
+	int currentplayer;
+	int win;
+	HumanPlayer player1;
+	HumanPlayer player2;
+	//int pl;
 	GamePP(void);
+	bool IsGameOver();
+	void SwitchTurn();
+
+public:
+	static GamePP* getInstance();
+	void init(std::vector<std::vector<int>>& board, int stplayer);
+	int move(std::vector<std::vector<int>>& board, int i, int j);
+	int getPlayer();
+	int GetWinner();
+	int getScore(int player);
 	~GamePP(void);
 };
-
